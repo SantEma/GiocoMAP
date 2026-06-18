b0d7d702cf4fd4baa530bf481443027d7198fa2a
Emanuele Santoruvo <e.santoruvo1@studenti.uniba.it>
2026-06-18 16:09:05 +0200

    Creazione dei vari packages

--- GiocoMAP/src/main/java/aeg/giocomap/GiocoMAP.java
+++ GiocoMAP/src/main/java/aeg/giocomap/GiocoMAP.java
@@ -9,8 +9,7 @@ package aeg.giocomap;
  * @author emanuele
  */
 public class GiocoMAP {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+/*
+Entrypoint del codice
+*/
 }
