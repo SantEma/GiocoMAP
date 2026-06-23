3aebba442ff775c0634d757582b3fca4004cbca1
Emanuele Santoruvo <e.santoruvo1@studenti.uniba.it>
2026-06-23 14:52:39 +0200

    Aggiunta dei packages con rispettivi file scheletro

---
+++ GiocoMAP/src/main/java/aeg/giocomap/View/MainFrame.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aeg.giocomap.View;
+
+/**
+ *
+ * @author emanu
+ */
+public class MainFrame {
+    /*
+    Da implementare
+    Teoricamente questo dovrebbe avere due sottoclassi di questa:
+    - ImagePanel: Pannello per quanto riguarda la visualiazzione di immagini
+    - DialogPanel: Pannello per i dialoghi e per i pulsanti
+    */
+}
